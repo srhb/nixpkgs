d978e8bde47be02d71a49d0abe334b5bc422316a
Anders Møldrup <anm@dbc.dk>
2017-12-05 10:01:44 +0000

    Changed logback.xml to STDOUT logging only

--- pkgs/servers/search/cerebro/default.nix
+++ pkgs/servers/search/cerebro/default.nix
@@ -14,10 +14,37 @@ stdenv.mkDerivation rec {
   installPhase = ''
     mkdir $out
     cp -r bin conf lib README.md $out/
+
     wrapProgram $out/bin/cerebro --set JAVA_HOME "${jre_headless}"
 
     substituteInPlace $out/conf/application.conf --replace 'data.path = "./cerebro.db"' 'data.path = "/tmp/cerebro.db"'
-    substituteInPlace $out/conf/logback.xml --replace '<file>''${application.home:-.}/logs/application.log</file>' '<file>/tmp/logs/cerebro.log</file>'
+
+    cat << EOF > $out/conf/logback.xml
+    <configuration>
+
+    <conversionRule conversionWord="coloredLevel" converterClass="play.api.libs.logback.ColoredLevel"/>
+
+    <appender name="STDOUT" class="ch.qos.logback.core.ConsoleAppender">
+        <encoder>
+            <pattern>%coloredLevel %logger{15} - %message%n%xException{5}</pattern>
+        </encoder>
+    </appender>
+
+    <logger name="play" level="INFO"/>
+    <logger name="application" level="INFO"/>
+
+    <!-- Off these ones as they are annoying, and anyway we manage configuration ourself -->
+    <logger name="com.avaje.ebean.config.PropertyMapLoader" level="OFF"/>
+    <logger name="com.avaje.ebeaninternal.server.core.XmlConfigLoader" level="OFF"/>
+    <logger name="com.avaje.ebeaninternal.server.lib.BackgroundThread" level="OFF"/>
+    <logger name="com.gargoylesoftware.htmlunit.javascript" level="OFF"/>
+
+    <root level="ERROR">
+        <appender-ref ref="STDOUT"/>
+    </root>
+
+    </configuration> 
+    EOF
 
   '';
 
