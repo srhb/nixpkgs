edbf00c30d7600fb278e23a5963249476053bfa9
Anders Møldrup <anm@dbc.dk>
2018-01-10 12:23:31 +0100

    Fixed another error in SHA

--- pkgs/development/tools/misc/kibana/plugins.nix
+++ pkgs/development/tools/misc/kibana/plugins.nix
@@ -27,8 +27,8 @@ in {
     pluginName = "kibana-readonlyrest";
     version = "1.16.14";
     src = fetchurl {
-      url = "https://artifactory.dbc.dk/artifactory/binary-platform/kibana/readonlyrest/readonlyrest_kbn_pro-1.16.14-20171211_es6.0.0.zip";
-      sha256 = "15kpmdvmiy2pj7laqxxprzb6rxmi0ckcdmsxjnnbcqvji40gmdcn";
+      url = "https://artifactory.dbc.dk/artifactory/binary-platform/kibana/readonlyrest/readonlyrest_kbn_pro-1.16.14-20180108_es6.0.0.zip";
+      sha256 = "956f7d411c7a2a839ebce70deed94f2f06cbcefc8e02dc6132978aac8013a38c";
       name = "readonlyrest_kbn_pro-${version}-20180108_es6.0.0.zip";
     };
 
