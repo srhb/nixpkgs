92f09ba2c97c8a2ce684d8d37ca1e22f86cf9d61
Anders Møldrup <anm@dbc.dk>
2018-01-10 15:17:35 +0100

    Updated elk6-ror test with test of kibana behind ror

--- nixos/tests/elk6-ror.nix
+++ nixos/tests/elk6-ror.nix
@@ -103,7 +103,9 @@ in {
 
     # See if kibana is healthy.
     $one->waitForUnit("kibana.service");
-    $one->waitUntilSucceeds("curl --silent --show-error 'http://localhost:5601/api/status' | jq .status.overall.state | grep green");
+    #$one->waitUntilSucceeds("curl --silent --show-error 'http://localhost:5601/api/status' | jq .status.overall.state | grep green");
+    $one->waitUntilSucceeds("curl --silent --show-error -d 'username=admin&password=dev&submit=true' -X POST 'localhost:5601/login?nextUrl=/api/status' -XPOST -H 'kbn-xsrf: api' -c cookie");
+    $one->waitUntilSucceeds("curl --silent --show-error -b cookie 'localhost:5601/api/status' | jq .status.overall.state | grep green");
 
     # See if logstash messages arive in elasticsearch.
     $one->waitUntilSucceeds("curl --silent --show-error '${esUrl}/_search' -H 'Content-Type: application/json' -d '{\"query\" : { \"match\" : { \"message\" : \"flowers\"}}}' | jq .hits.total | grep -v 0");
