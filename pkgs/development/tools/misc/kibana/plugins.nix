1ae4ebbee94bb55c5c8b509037c39aa49412d9ab
Anders Møldrup <anm@dbc.dk>
2018-01-10 10:25:33 +0100

    Fixed an error in SHA string

--- pkgs/development/tools/misc/kibana/plugins.nix
+++ pkgs/development/tools/misc/kibana/plugins.nix
@@ -28,7 +28,7 @@ in {
     version = "1.16.14";
     src = fetchurl {
       url = "https://artifactory.dbc.dk/artifactory/binary-platform/kibana/readonlyrest/readonlyrest_kbn_pro-1.16.14-20171211_es6.0.0.zip";
-      sha256 = "956f7d411c7a2a839ebce70deed94f2f06cbcefc8e02dc6132978aac8013a38c";
+      sha256 = "15kpmdvmiy2pj7laqxxprzb6rxmi0ckcdmsxjnnbcqvji40gmdcn";
       name = "readonlyrest_kbn_pro-${version}-20180108_es6.0.0.zip";
     };
 
