328d65949cb701f2661a30a399afbd6d8d64099d
Erik Skytthe <ers@dbc.dk>
2018-01-12 16:22:18 +0100

    Added and tested parameteres for exporter

--- nixos/modules/services/monitoring/prometheus/elasticsearch-exporter.nix
+++ nixos/modules/services/monitoring/prometheus/elasticsearch-exporter.nix
@@ -1,3 +1,4 @@
+# prometheus elasticsearch exporter
 { config, pkgs, lib, ... }:
 
 with lib;
@@ -7,45 +8,45 @@ let
 in {
   options = {
     services.prometheus.elasticsearchExporter = {
-      enable = mkEnableOption "Whether to enable prometheus elasticsearch exporter";
+      enable = mkEnableOption "prometheus elasticsearch exporter";
 
       es.all = mkOption {
         type = types.bool;
         default = false;
-        description = ''
-      	  Export stats for all nodes in the cluster
-        '';
+        description = "Export stats for all nodes in the cluster";
       };
       es.ca = mkOption {
-        description = "Path to PEM file that conains trusted CAs for the Elasticsearch connection"
+        description = "Path to PEM file that conains trusted CAs for the Elasticsearch connection";
         default = "";
         type = types.str;
       };
       es.client-cert = mkOption {
-        description = "Path to PEM file that conains the corresponding cert for the private key to connect to Elasticsearch"
+        description = "Path to PEM file that conains the corresponding cert for the private key to connect to Elasticsearch";
+        default = "";
         type = types.str;
       };
       es.client-private-key = mkOption {
-        description = "Path to PEM file that conains the private key for client auth when connecting to Elasticsearch"
+        description = "Path to PEM file that conains the private key for client auth when connecting to Elasticsearch";
+        default = "";
         type = types.str;
       };
       es.timeout = mkOption {
-        description = "Timeout for trying to get stats from Elasticsearch"
+        description = "Timeout for trying to get stats from Elasticsearch";
         default = "5s";
-        type = types.int;
+        type = types.str;
       };
       es.uri = mkOption {
-        description = "HTTP API address of an Elasticsearch node"
-        default = ""http://localhost:9200";
+        description = "HTTP API address of an Elasticsearch node";
+        default = "http://localhost:9200";
         type = types.str;
       };
       web.listen-address = mkOption {
-        description = "Address to listen on for web interface and telemetry"
+        description = "Address to listen on for web interface and telemetry";
         default = ":9108";
         type = types.str;
       };
       web.telemetry-path = mkOption {
-        description = "Path under which to expose metrics"
+        description = "Path under which to expose metrics";
         default = "/metrics";
         type = types.str;
       };
@@ -59,12 +60,20 @@ in {
       unitConfig.Documentation = "https://github.com/justwatchcom/elasticsearch_exporter";
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
-      script = ''
-        exec ${pkgs.prometheus-elasticsearch-exporter}/bin/elasticsearch_exporter \
-        -Dhttp.port=${toString cfg.port}
-      '';
 
       serviceConfig = {
+        ExecStart = ''
+          ${pkgs.prometheus-elasticsearch-exporter}/bin/elasticsearch_exporter \
+          -es.timeout ${cfg.es.timeout} \
+          -es.uri ${cfg.es.uri} \
+          -web.listen-address ${cfg.web.listen-address} \
+          -web.telemetry-path ${cfg.web.telemetry-path} \
+          ${optionalString (cfg.es.all == "true") ''-es.all''} \
+          ${optionalString (cfg.es.ca != "") ''-es.ca ${cfg.es.ca}''} \
+          ${optionalString (cfg.es.client-cert != "") ''-es.client-cert ${cfg.es.client-cert}''} \
+          ${optionalString (cfg.es.client-private-key != "") ''-es.client-private-key ${cfg.es.client-private-key}''} 
+        '';
+
         User = "nobody";
         Restart = "always";
         PrivateTmp = true;
