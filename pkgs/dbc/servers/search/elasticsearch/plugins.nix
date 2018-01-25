09cb164186dafa06a620dee38ae7dde5d734211f
Sarah Brofeldt <sbrofeldt@gmail.com>
2018-01-25 09:54:11 +0100

    elk6_dbc: patch RoR java security policy to allow reading from store

--- pkgs/dbc/servers/search/elasticsearch/plugins.nix
+++ pkgs/dbc/servers/search/elasticsearch/plugins.nix
@@ -1,27 +1,9 @@
-{ pkgs, stdenv, fetchurl, unzip, elasticsearch }:
+{ pkgs, stdenv, fetchurl, unzip, elasticsearch, zip }:
 
 with pkgs.lib;
 
-let
-  esPlugin = a@{
-    pluginName,
-    installPhase ? ''
-      mkdir -p $out/bin
-      ES_HOME=$out ${elasticsearch}/bin/elasticsearch-plugin --install ${pluginName} --url file://$src
-    '',
-    ...
-  }:
-    stdenv.mkDerivation (a // {
-      inherit installPhase;
-      unpackPhase = "true";
-      buildInputs = [ unzip ];
-      meta = a.meta // {
-        platforms = elasticsearch.meta.platforms;
-        maintainers = (a.meta.maintainers or []) ++ [ maintainers.offline ];
-      };
-    });
-in {
-  elasticsearch_readonlyrest = esPlugin rec {
+{
+  elasticsearch_readonlyrest = stdenv.mkDerivation rec {
     name = "elasticsearch-readonlyrest-${version}";
     pluginName = "elasticsearch-readonlyrest";
     version = "1.16.15_es6.1.1";
@@ -29,6 +11,15 @@ in {
       url = "https://artifactory.dbc.dk/artifactory/binary-platform/elasticsearch/readonlyrest/readonlyrest-${version}.zip";
       sha256 = "1zbmmlps5b95dbdb7ppph58q5ij8160dwg6baxhrlch1dzvg2sbi";
     };
+
+    buildInputs = [ zip unzip ];
+    patches = [ ./ror-policy.patch ];
+
+    installPhase = ''
+      zip -r out.zip .
+      mkdir -p $out/bin
+      ES_HOME=$out ${elasticsearch}/bin/elasticsearch-plugin --install ${pluginName} --url file://$(readlink -e out.zip)
+    '';
 
     meta = {
       homepage = https://github.com/sscarduzio/elasticsearch-readonlyrest-plugin;
