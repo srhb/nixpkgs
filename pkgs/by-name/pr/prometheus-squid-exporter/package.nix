411a01e11079e818b936e4c5fe4f3d6f73500969
Sarah Brofeldt <sarah@qtr.dk>
2024-05-15 12:17:14 +0200

    prometheus-squid-exporter: init at 1.11.0

---
+++ pkgs/by-name/pr/prometheus-squid-exporter/package.nix
@@ -0,0 +1,23 @@
+{ lib, buildGoModule, fetchFromGitHub, }:
+
+buildGoModule rec {
+  pname = "squid-exporter";
+
+  version = "1.11.0";
+
+  src = fetchFromGitHub {
+    owner = "boynux";
+    repo = "squid-exporter";
+    rev = "v${version}";
+    sha256 = "sha256-43f6952IqUHoB5CN0p5R5J/sMKbTe2msF9FGqykwMBo=";
+  };
+
+  vendorHash = null;
+
+  meta = with lib; {
+    description = "Squid Prometheus exporter";
+    homepage = "https://github.com/boynux/squid-exporter";
+    license = licenses.mit;
+    maintainers = with maintainers; [ srhb ];
+  };
+}
